f0dee927eae4d3d43e1ebfe9df0eac9b4ab9cb37
Louis Stevens <loustevenshere@gmail.com>
2021-01-14 21:27:05 -0500

    almost done

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,7 +14,7 @@ const ContactForm = () => {
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name*</label>
+          <label htmlFor="firstName">First Name</label>
           <input
             name="firstName"
             placeholder="Edd"
@@ -26,7 +26,7 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="lastName">Last Name*</label>
+          <label htmlFor="lastName">Last Name</label>
           <input
             id="lastName"
             name="lastName"
